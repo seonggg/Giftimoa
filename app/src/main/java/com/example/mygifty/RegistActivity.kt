9e9f1cc30cfd3a60d6248d319d42401c88f071f0
JasmineKim-0418 <gkdmsdl9804@gmail.com>
2021-08-01 00:54:13 +0900

    add photo upload function

--- app/src/main/java/com/example/mygifty/RegistActivity.kt
+++ app/src/main/java/com/example/mygifty/RegistActivity.kt
@@ -5,6 +5,7 @@ import android.content.res.AssetManager
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -28,6 +29,7 @@ class RegistActivity : AppCompatActivity() {
     lateinit var place_edit : EditText
     lateinit var memo_edit: EditText
 
+    val Gallery= 1
     //이미지에서 추출한 날짜 변수
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,16 +49,43 @@ class RegistActivity : AppCompatActivity() {
         regist_img=findViewById(R.id.regist_img)
         regist_img.setOnClickListener({
             //이미지 업로드 코드
+            loadImage()
         })
 
         //텍스트추출 시험 실행
     }
 
+    private fun loadImage(){
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+
+        startActivityForResult(Intent.createChooser(intent,"Load Picture"),Gallery)
+    }
 
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.regist_menu,menu)
         return true
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == Gallery){
+            if(resultCode == RESULT_OK){
+                var dataUri = data?.data
+                try{
+                    var bitmap : Bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver,dataUri)
+                    regist_img.setImageBitmap(bitmap)
+                }catch (e:Exception){
+                    Toast.makeText(this,"$e",Toast.LENGTH_SHORT).show()
+                }
+            }
+            else{
+                //something wrong
+            }
+        }
     }
 
 
